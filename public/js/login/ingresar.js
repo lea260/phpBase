66a4d10e76ab0ddd319ef4666938ea3a6de456b8
leandro <lcalvetti1@gmail.com>
2022-05-23 19:18:08 -0300

    tercer commit

--- public/js/login/ingresar.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-  $(document).ready(function() {
-    //alert('ingresar');
-    let token= $("#token").val();
-    localStorage.setItem("token", token);
-  });//end ready
-})(jQuery);
